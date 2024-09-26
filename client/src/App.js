d01cef7817e00617358eb01d374097eab4f5ebb0
Soiconz <165038091+Soiconz@users.noreply.github.com>
2024-09-26 21:58:07 +0700

    THANG

--- client/src/App.js
+++ client/src/App.js
@@ -31,7 +31,7 @@ import BookingModal from './components/AirplaneComponent/BookingModal/BookingMod
 function App() {
   const userId = localStorage.getItem("userId");
 
-  // Chức năng thông báo 
+
   const handleError = (errorMessage) => {
     toast.error(errorMessage);
   };
